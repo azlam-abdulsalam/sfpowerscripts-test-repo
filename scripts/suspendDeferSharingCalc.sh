25702cbb6c7d670662247d36f86e543c1978def7
Azlam <43767972+azlam-abdulsalam@users.noreply.github.com>
2022-04-12 06:51:09 +1000

    retries to 3

--- scripts/suspendDeferSharingCalc.sh
+++ scripts/suspendDeferSharingCalc.sh
@@ -1,7 +1,7 @@
 # $1 - package
 # $2 - target org
 
-export BROWSERFORCE_RETRY_MAX_RETRIES=10
+export BROWSERFORCE_RETRY_MAX_RETRIES=3
 
 # Suspend defer sharing rules calculation
 sfdx browserforce:apply -f ./scripts/browserforceDefinitions/suspendSharingCalc.json -u $2
