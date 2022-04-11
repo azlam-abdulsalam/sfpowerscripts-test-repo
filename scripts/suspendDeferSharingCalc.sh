6673b753e045fe5b9cd34b32e7c107413624827a
azlam-abdulsalam <azlam.abdulsalam@accenture.com>
2022-04-12 07:34:29 +1000

    reduce retry to 1

--- scripts/suspendDeferSharingCalc.sh
+++ scripts/suspendDeferSharingCalc.sh
@@ -1,7 +1,7 @@
 # $1 - package
 # $2 - target org
 
-export BROWSERFORCE_RETRY_MAX_RETRIES=3
+export BROWSERFORCE_RETRY_MAX_RETRIES=1
 
 # Suspend defer sharing rules calculation
 sfdx browserforce:apply -f ./scripts/browserforceDefinitions/suspendSharingCalc.json -u $2
